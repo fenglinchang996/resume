939908d5b5184b9f5174568b4d4298205a3c5514
FLC <paulespling@gmail.com>
2022-06-05 22:04:11 +0800

    chore: add path alias

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
@@ -11,6 +12,9 @@ const config = {
 
   kit: {
     adapter: adapter(),
+    alias: {
+      $components: path.resolve('./src/components'),
+    },
   },
 };
 
