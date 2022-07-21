debde4302b6991f6cdcb093c4799f96ee490eef6
FLC <paulespling@gmail.com>
2022-07-21 19:23:27 +0800

    fix: typo

--- src/components/About/Summary.svelte
+++ src/components/About/Summary.svelte
@@ -1,8 +1,8 @@
 <div class="[hyphens:auto]">
   Front-end engineer with 2 years experience developing and maintaining
-  responsive websites. Familiar with web developement basics: HTML, CSS and
-  JavaScript. Also experienced in modern front-end framworks and libraries, such
-  as React, Redux, TypeScript and styled-components. Seeking a role as a
+  responsive websites. Familiar with web development basics: HTML, CSS and
+  JavaScript. Also experienced in modern front-end frameworks and libraries,
+  such as React, Redux, TypeScript and styled-components. Seeking a role as a
   front-end engineer with a passion for creating immersive user experiences and
   developing high performance websites.
 </div>
