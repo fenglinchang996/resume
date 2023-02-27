2bfcdc9d34dd2b800f6fdbb4bab007863a2a724d
FLC <paulespling@gmail.com>
2023-02-27 12:13:05 +0800

    feat: add Toolbar

---
+++ src/components/Toolbar/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import LanguageSelect from './LanguageSelect.vue';
+import PagePrint from './PagePrint.vue';
+</script>
+
+<template>
+  <div class="flex gap-4">
+    <LanguageSelect />
+    <PagePrint />
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,15 @@
 <script setup lang="ts">
-import LanguageSelect from '@/components/common/LanguageSelect.vue';
 import About from '@/components/About/index.vue';
 import Skills from '@/components/Skills/index.vue';
 import Experience from '@/components/Experience/index.vue';
 import Project from '@/components/Projects/index.vue';
 import Education from '@/components/Education/index.vue';
-import PagePrint from '@/components/common/PagePrint.vue';
+import Toolbar from '@/components/Toolbar/index.vue';
 </script>
 
 <template>
   <main class="relative mx-auto px-6 py-4 max-w-screen-lg md:pt-8">
-    <div class="absolute top-4 right-6 md:top-8 flex gap-4 print:hidden">
-      <LanguageSelect />
-      <PagePrint />
-    </div>
+    <div class="absolute top-4 right-6 md:top-8 print:hidden"><Toolbar /></div>
     <About />
     <!-- mobile -->
     <div class="lg:hidden print:hidden">
