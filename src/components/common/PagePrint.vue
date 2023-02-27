bb06ff33b201fc7e3f85596ddd38c4156dcb441d
FLC <paulespling@gmail.com>
2023-02-27 11:54:45 +0800

    chore: remove uncessary files

--- src/components/common/PagePrint.vue
+++ src/components/common/PagePrint.vue
@@ -1,9 +1,9 @@
+<script setup lang="ts">
+const print = () => window.print();
+</script>
+
 <template>
   <button @click="print">
     <i class="fa-solid fa-print text-gray-600"></i>
   </button>
 </template>
-
-<script setup lang="ts">
-const print = () => window.print();
-</script>
