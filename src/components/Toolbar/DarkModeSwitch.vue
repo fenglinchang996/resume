a2cc8ebbf9149ff7429ab4441cf6eaa9dcf92c65
FLC <paulespling@gmail.com>
2023-03-01 23:31:49 +0800

    feat: add dark mode switch

---
+++ src/components/Toolbar/DarkModeSwitch.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+
+const isToggled = ref(false);
+watch(isToggled, (isToggled) => {
+  if (isToggled) {
+    window.document.documentElement.classList.add('dark');
+  } else {
+    window.document.documentElement.classList.remove('dark');
+  }
+});
+</script>
+<template>
+  <div class="flex items-center">
+    <input
+      type="checkbox"
+      id="switch"
+      v-model="isToggled"
+      class="h-0 w-0 invisible"
+    />
+    <label
+      for="switch"
+      class="inline-block relative w-10 h-5 cursor-pointer border border-gray-300 bg-gray-200 dark:bg-gray-700 shadow-lg rounded-full"
+    >
+      <span
+        class="absolute top-1/2 -translate-y-1/2 right-full translate-x-full dark:right-0 dark:translate-x-0 duration-1000"
+      >
+        <span
+          class="flex justify-center items-center rounded-full bg-white dark:bg-gray-900 w-4 h-4 text-xs"
+        >
+          <span class="text-gray-600 dark:hidden">
+            <i class="fa-regular fa-sun"></i>
+          </span>
+          <span class="text-gray-100 hidden dark:inline">
+            <i class="fa-regular fa-moon"></i>
+          </span>
+        </span>
+      </span>
+    </label>
+  </div>
+</template>
--- src/components/Toolbar/index.vue
+++ src/components/Toolbar/index.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import LanguageSelect from './LanguageSelect.vue';
 import PagePrint from './PagePrint.vue';
+import DarkModeSwitch from './DarkModeSwitch.vue';
 </script>
 
 <template>
   <div class="flex items-center gap-4">
     <LanguageSelect />
     <PagePrint />
+    <DarkModeSwitch />
   </div>
 </template>
