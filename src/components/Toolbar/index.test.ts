4059c3e87f1e2343318307f169c747df83c4bc9c
FLC <paulespling@gmail.com>
2023-06-15 22:53:31 +0800

    refactor: use mount with shallow property instead of shallowMount in testing

--- src/components/Toolbar/index.test.ts
+++ src/components/Toolbar/index.test.ts
@@ -1,12 +1,12 @@
 import { describe, test, expect } from 'vitest';
-import { shallowMount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import Toolbar from './index.vue';
 import LanguageSelect from './LanguageSelect.vue';
 import PagePrint from './PagePrint.vue';
 import ThemeAppearanceSwitch from '@/components/common/ThemeAppearanceSwitch.vue';
 
 describe('Toolbar component', () => {
-  const wrapper = shallowMount(Toolbar);
+  const wrapper = mount(Toolbar, { shallow: true });
   test('child component', () => {
     expect(wrapper.findComponent(LanguageSelect)).toBeDefined();
     expect(wrapper.findComponent(PagePrint)).toBeDefined();
