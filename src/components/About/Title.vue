87f5e722f777f072b79341ec4b49fa3146cd8a10
FLC <paulespling@gmail.com>
2023-06-24 09:40:53 +0800

    refactor: remove unnecessary span tag in heading tag

--- src/components/About/Title.vue
+++ src/components/About/Title.vue
@@ -7,11 +7,7 @@ defineProps<{
 
 <template>
   <div class="space-y-1 md:flex gap-x-4 items-center print:flex">
-    <h1>
-      <span class="text-3xl font-bold">{{ name }}</span>
-    </h1>
-    <h2>
-      <span class="text-2xl">{{ headline }}</span>
-    </h2>
+    <h1 class="text-3xl font-bold">{{ name }}</h1>
+    <h2 class="text-2xl">{{ headline }}</h2>
   </div>
 </template>
